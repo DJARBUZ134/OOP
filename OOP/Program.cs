6b7f83befcd271297c5d6c4edef57b8b5487ba3e
Kotsuda <artyomplotnikov2007@gmail.com>
2025-05-12 20:05:18 +0500

    1

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -7,47 +7,50 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
-    class Book : Document,IPrintable
+    public class Book : Document,IPrintable
     {
-        private string _title 
+        private string title = "Under";
+        private string author = "Under";
+        private int year = 1801;
+
+        public string _title 
         {
             get
             {
-                return _title;
+                return title;
             }
             set
             {
-                _title = value;
+               title = value;
             }
         }
-        private string _author 
+        public string _author 
         {
             get
             {
-                return _author;
+                return author;
             }
             set
             {
-                _author = value;
+                author = value;
             }
         }
-        private int _year 
+        public int _year 
         {
             get
             {
-                return _year;
+                return year;
             }
             set
             {
-                if(value > 1800)
-                {
-                    _year = value;
-                }
-                else
-                {
-                    Console.WriteLine("Значение должно быть больше 1800");
-                }
+                    year = value;
             }
+        }
+        public Book()
+        {
+            _title = "ndan";
+            _author ="fas ";
+            _year =1801;
         }
         public Book(string title, string author, int year)
         {
@@ -63,7 +66,6 @@ namespace OOP
             throw new NotImplementedException();// тут короче код
         }
 
-
         public string PrintInfo()
         {
             throw new NotImplementedException();// тут короче код
@@ -75,33 +77,55 @@ namespace OOP
         }       
     }
 
-    class Library
-    {
-        static List<Book> list_book = new List<Book>();
+    public class Library : Book
+    {        
+        private List<Book> list_book = new List<Book>();
+
         public void AddBook(Book book)
         {
-            Console.Write("Введите название книги которую хотите добавить: ");
-            string add_name_book = Convert.ToString(Console.ReadLine());
+            Program obj = new Program();
+            Console.Write("Введите свединие о книги которую хотите добавить: ");
+
+            Console.WriteLine("Введите автора");
+            string add_author = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Введите название");
+            string add_title = Convert.ToString(Console.ReadLine());
+
+
+            // Проверка конвертирования, Не добавлять буквы, Не меньше 1800
+            Console.WriteLine("Введите год");
+            int add_year = Convert.ToInt32(Console.ReadLine());
+
+            Book add_name_book = new Book(add_author, add_title, add_year);
+
             Console.WriteLine("Подтвердите что хотите добавить данную книгу (y/n)");
             string add_confirm = Convert.ToString(Console.ReadKey());
+
             if (add_confirm.ToLower() == "y")
             {
-                if (list_book.Contains(Convert.ToString(add_name_book)) == false)
+                if (list_book.Contains(add_name_book) == false)
+
                     list_book.Add(add_name_book);
-                else
-                    Console.WriteLine("Книга с таким названием уже имеется в библиотеке");
             }
+            else
+            {
+                Console.WriteLine("Книга с таким названием уже имеется в библиотеке");
+            }
+
+            obj.Meth();
         }
+    
         static void RemoveBook(string title) 
         {
             Console.Write("Введите название книги которую хотите удалить: ");
             string delete_name_book = Convert.ToString(Console.ReadLine());
             Console.WriteLine("Подтвердите что хотите удалить данную книгу (y/n)");
             string delete_confirm = Convert.ToString(Console.ReadKey());
-            if (delete_confirm.ToLower() == "y" && delete_name_book.ToLower() == )
-            {
+            //if (delete_confirm.ToLower() == "y" && delete_name_book.ToLower() == )
+            //{
 
-            }
+            //}
         }
         static void FindBooksByAuthor(string author)
         {
@@ -114,15 +138,57 @@ namespace OOP
 
         }
     }
-    abstract class Document
+
+    public abstract class Document
     {
         int id;
         string title;
         public abstract string GetDocumentType();
 
     }
+
     interface IPrintable
     {
         string PrintInfo();
+    }
+
+    class Program
+    {
+
+        public void Meth()
+        {
+            Console.Clear();
+            Main();
+        }
+
+        public static void Main()
+        {
+            Library obj = new Library();
+            Book book1 = new Book();
+
+            Console.WriteLine("Выберите действие: ");
+            Console.WriteLine("1 - Добавить книгу ");
+            Console.WriteLine("2 - Кол-во книг в библиотеке ");
+            Console.WriteLine("3 - Найти книгу по автору ");
+            Console.WriteLine("4 - Удалить книгу по названию");
+            Console.WriteLine("5 - Просмотр всех книг ");
+
+            string devst = Console.ReadLine();
+            
+            switch (devst)
+            {
+                case "1":
+                    { 
+                        obj.AddBook(book1);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Нет действия с данным номером");
+                        break;
+                    }
+            }
+        }
+
     }
 }
